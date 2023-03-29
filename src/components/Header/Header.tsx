a063915efc66402e2737c727eedf603c78d81c97
SVMPLR <svmplrofficial@gmail.com>
2023-03-29 19:49:11 +0200

    Test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,9 @@ import LogoBrandLight from "@/public/images/logo/loopgate-brand-light.svg";
 const Header = () => {
   return (
     <div className="w-full flex py-3 px-4 sm:px-6 sticky top-0 items-center justify-between border-b sm:border-b-0 border-white/10 bg-slate-900/50 backdrop-blur-lg z-10">
-      <Image src={LogoBrandLight} alt="SVMPLR Logo" height={32} />
-
+      <a href="https://svmplr.com">
+        <Image src={LogoBrandLight} alt="SVMPLR logo" height={32} />
+      </a>
       <ConnectKitButton />
     </div>
   );
