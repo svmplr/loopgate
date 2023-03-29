5ed82d4189bc4bc8ed605c1489da11cfe3f8e6cf
SVMPLR <svmplrofficial@gmail.com>
2023-03-29 22:35:01 +0200

    test

--- src/components/Fonts/Fonts.ts
+++ src/components/Fonts/Fonts.ts
@@ -3,11 +3,9 @@ import { Inter, Unbounded } from "@next/font/google";
 export const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
-  weight: "100"
 });
 
 export const unbounded = Unbounded({
   subsets: ["latin"],
-  weight: "700",
   variable: "--font-unbounded",
 });
