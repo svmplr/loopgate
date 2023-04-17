0e2c36a75958d2ee5ece0ac40a2219386ae3030d
svmplr <129234593+svmplr@users.noreply.github.com>
2023-04-17 12:37:41 +0200

    Post Revwind

--- src/config/config.ts
+++ src/config/config.ts
@@ -52,18 +52,181 @@ const unlockables: Unlockable[] = [
     ], // You need to own 'GM #1' to unlock this.
   },
   {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
+    cid: "bafybeiftvauq25nidsk5waxnmn62gqqwpgctu3j7vsbb5t4grklgrwpd6e", // Remedy
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
+      "0x95629a13d2bb9d89927f5d4a08a721ab1b6c4eb22ed4891b09fd4dbdd47360f3",
+    ],
   },
   {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
+    cid: "bafybeibsdybfl7jcb4oqbalcppgy2zgbypcpu3sxizjmbqg7eppdx7nqma", // Groove
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+      "0x1c24a69f4cc50ade39d05b29c93a2ea6dd46131d94b90df388c74f1f7f5f6cda",
+    ],
   },
+  {
+    cid: "bafybeihwiavg4qmbve3dihhl6fvova5gw5j3ywj2vedmr6nfswn4x4b454", // No Sleep
+    nftId: [
+      "0xe61495b628bcd46414e6145c50a89cc9833a15689a3e8e8481339f33f563f651",
+    ],
+  },
+  {                  // BONUS - SOUNDS OF SVMPLR - Running Up That Hill //
+    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Running Up That Hill
+    nftId: [
+      "0x6c2da065b29c6fce4dbf81184c9cad97320513502c22c80ecfcd4144c5963daf",
+      "0x95629a13d2bb9d89927f5d4a08a721ab1b6c4eb22ed4891b09fd4dbdd47360f3",
+      "0x1c24a69f4cc50ade39d05b29c93a2ea6dd46131d94b90df388c74f1f7f5f6cda",
+      "0xe61495b628bcd46414e6145c50a89cc9833a15689a3e8e8481339f33f563f651",
+    ],
+  },
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  {                              // BEATS BY SVMPLR //
+    cid: "bafybeie5ojsmfx6rq3qqcd4qh72ln3j2ay55q5pujey4wfvbcx6bhcsyv4", // Chicago
+    nftId: [
+      "0xca82f5764f8b798cfff469e7c227c411fc7a039c6ab65e886297b5ffcc2a6b2e",
+    ],
+  },
+  {
+    cid: "bafybeif2dyq3zbfguwhdjbjlufajuqsxn6kun6yxz7a7gf7k6cn7le3r5y", // New Delhi
+    nftId: [
+      "0x147c00940e85030e761168c0d114690ddc212a5deece7ea72d5ba63fa6033a35",
+    ],
+  },
+  {
+    cid: "bafybeihldcabl5pq6pz3olxkeqlcm5i7x7yitf34qrgq5kmjlgsd5rarw4", // Ottawa
+    nftId: [
+      "0xb54b0bd6c73ed02cd5cce5e80b84a31f04d8c3bcb291482a7cd75456c5f596e9",
+    ],
+  },
+  {
+    cid: "bafybeigelx5nrmgrztrrcmrcmnp4rveggu4gzypx2ro5mw43kl556ekiei", // Paris
+    nftId: [
+      "0x1c14de35a795e58ad136a2390384a8e167c660c877dc6f64190a580bfb5d153b",
+    ],
+  },
+  {
+    cid: "bafybeie7bknujass7xhyolaogbqzysw2lstfaudatqbakjsemrezch3n74", // Los Angeles
+    nftId: [
+      "0x3a0a32cc2dff5523cf8191ba456ba909d8928b1d412d9244831970629100303c",
+    ],
+  },
+  {
+    cid: "bafybeigqsljz25hdmi4nk3vtj2tetkf2pnnwdka47fcxdda2drxidh6v6a", // New York
+    nftId: [
+      "0x2156b02205aa3112937875cc09cfa5540555b09c765d936390fa97fad99f6040",
+    ],
+  },
+  {
+    cid: "bafybeigfcufdo57gs3ofibpuhjyjbeusoft67smdnmbkzflpjpwc2am4ay", // Seoul
+    nftId: [
+      "0x42a9e416d3ce9cf023d151869a3939535227b4f4abb0785b9abc5fccebfc2645",
+    ],
+  },
+  {
+    cid: "bafybeigqvohou26whbqmyhinmcuwnt5xxb6vpvhgtkllhwm3mjiddocspq", // Stockholm
+    nftId: [
+      "0xcb322d249745d385d63232d844ddb2f997642f0a3166a2119e2d45e1df865bc0",
+    ],
+  },
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  {                              // World of SVMPLR //
+    cid: "bafybeidpezi3vj7kwmxlz4apqi2exdlcpkiyrroxilo7pmjxajvsualcfq", // Shanti (Code)
+    nftId: [
+      "0xfee43e951635858d077b45de6d2c67919be5d76ee8829c45171292ec9e19e143",
+    ],
+  },
+  {                              // MIXTAPE - RORSCHACH //
+    cid: "bafybeiecnv2x62bmtdkswmc6qjdptssjr6jzi4ksw4yzu457yw6md3hyua", // Rorschach
+    nftId: [
+      "0x1703cb457a2c270c8e016f26a0295aafa6356b2cbd11cbb6b1daa0cca0a9e20e",
+    ],
+  },
+  {                           // BONUS DJ SET - RORSCHACH //
+    cid: "bafybeignizscfldiumlj4puqwqpyqd67jk6ufqlg6lmfzy2t6bzyctdf2q", // Rorschach
+    nftId: [
+      "0x1703cb457a2c270c8e016f26a0295aafa6356b2cbd11cbb6b1daa0cca0a9e20e",
+    ],
+  },    
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    {                    // BONUS DJ SET FOR ALL (MINUS SHANTI) //
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Waiting
+      nftId: [
+        "0x6c2da065b29c6fce4dbf81184c9cad97320513502c22c80ecfcd4144c5963daf",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Remedy
+      nftId: [
+        "0x95629a13d2bb9d89927f5d4a08a721ab1b6c4eb22ed4891b09fd4dbdd47360f3",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Groove
+      nftId: [
+        "0x1c24a69f4cc50ade39d05b29c93a2ea6dd46131d94b90df388c74f1f7f5f6cda",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // No Sleep
+      nftId: [
+        "0xe61495b628bcd46414e6145c50a89cc9833a15689a3e8e8481339f33f563f651",
+      ],
+    },
+    {                              // BEATS BY SVMPLR //
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Chicago
+      nftId: [
+        "0xca82f5764f8b798cfff469e7c227c411fc7a039c6ab65e886297b5ffcc2a6b2e",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // New Delhi
+      nftId: [
+        "0x147c00940e85030e761168c0d114690ddc212a5deece7ea72d5ba63fa6033a35",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Ottawa
+      nftId: [
+        "0xb54b0bd6c73ed02cd5cce5e80b84a31f04d8c3bcb291482a7cd75456c5f596e9",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Paris
+      nftId: [
+        "0x1c14de35a795e58ad136a2390384a8e167c660c877dc6f64190a580bfb5d153b",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Los Angeles
+      nftId: [
+        "0x3a0a32cc2dff5523cf8191ba456ba909d8928b1d412d9244831970629100303c",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // New York
+      nftId: [
+        "0x2156b02205aa3112937875cc09cfa5540555b09c765d936390fa97fad99f6040",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Seoul
+      nftId: [
+        "0x42a9e416d3ce9cf023d151869a3939535227b4f4abb0785b9abc5fccebfc2645",
+      ],
+    },
+    {
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Stockholm
+      nftId: [
+        "0xcb322d249745d385d63232d844ddb2f997642f0a3166a2119e2d45e1df865bc0",
+      ],
+    },
+    {                              // MIXTAPE - RORSCHACH //
+      cid: "bafybeiag3t5nbtpdlbnwhubn4q2ebu6k5wre2lynpbnfwfofyteqrkl7pe", // Rorschach
+      nftId: [
+        "0x1703cb457a2c270c8e016f26a0295aafa6356b2cbd11cbb6b1daa0cca0a9e20e",
+      ],
+    },
 ];
 
 export { unlockables };
