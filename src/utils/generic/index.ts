a2a23f70c15a62c3cddf5b59e4345f1bf9585593
svmplr <129234593+svmplr@users.noreply.github.com>
2023-04-18 23:00:10 +0200

    Edit containsalltest to contain any instead

--- src/utils/generic/index.ts
+++ src/utils/generic/index.ts
@@ -1,7 +1,6 @@
-import checkIfContainsAll from "./checkIfContainsAll";
 import checkIfContainsAny from "./checkIfContainsAny";
 import findUnlockedCids from "./findUnlockedCids";
 import getCurrentYear from "./getCurrentYear";
 import cn from "./cn";
 
-export { checkIfContainsAll, checkIfContainsAny, findUnlockedCids, getCurrentYear, cn };
+export { checkIfContainsAny, findUnlockedCids, getCurrentYear, cn };
