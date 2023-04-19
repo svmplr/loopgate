98f34b933990fdbb72f9d274a27ddf0fed317aef
svmplr <129234593+svmplr@users.noreply.github.com>
2023-04-19 09:36:30 +0200

    test6

--- src/components/ConnectedPage/UnlockLink.tsx
+++ src/components/ConnectedPage/UnlockLink.tsx
@@ -7,20 +7,18 @@ import {
 interface Props {
   title: string;
   unlockUrl: string;
-  cid: "Download";
+  cid: `baf${string}`;
 }
 
 const UnlockLink = ({ title, unlockUrl, cid }: Props) => {
-  const fileExtension = unlockUrl.substring(unlockUrl.lastIndexOf(".") + 1);
-
   return (
     <a
-     className="flex space-x-4 items-center w-full border-t border-white/20 py-4 hover:bg-white/10 duration-150 px-2 group text-white/80 hover:text-white-100"
-     href={unlockUrl}
-     target="_blank"
-     rel="noreferrer"
-     download={`${title}.${fileExtension}`}
-       >
+      className="flex space-x-4 items-center w-full border-t border-white/20 py-4 hover:bg-white/10 duration-150 px-2 group text-white/80 hover:text-white-100"
+      href={unlockUrl}
+      target="_blank"
+      rel="noreferrer"
+      download={process.env.DOWNLOAD_NAME}
+>
       <div className="bg-sky-500/70 group-hover:bg-sky-500 rounded-md h-8 w-8 flex-shrink-0 text-slate-900 duration-150 relative overflow-hidden">
         <LockClosedIcon className="absolute h-full w-full left-0 top-0 p-1.5 duration-150 ease-in-out group-hover:translate-y-full" />
         <LockOpenIcon className="absolute h-full w-full left-0 top-0 p-1.5 duration-150 ease-in-out -translate-y-full group-hover:translate-y-0" />
