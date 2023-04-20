1a58fe3da3ce60758f4a6b6dfc75e16850b06e19
svmplr <129234593+svmplr@users.noreply.github.com>
2023-04-20 20:16:29 +0200

    test

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="py-4 px-8 w-full flex flex-col md:flex-row items-center justify-between space-y-1 md:space-y-0 mt-auto text-center">
       <p className="text-white/40">
-        <FooterLink href="https://svmplr.com"></FooterLink>
+        <FooterLink href="https://svmplr.com">.</FooterLink>
       </p>
     </div>
   );
