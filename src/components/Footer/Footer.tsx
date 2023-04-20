78b7cf4201321e08734b32f6724a18862d690d03
svmplr <129234593+svmplr@users.noreply.github.com>
2023-04-20 20:08:31 +0200

    rollback

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,34 +1,13 @@
 import FooterLink from "./FooterLink";
-import { TwitterIcon, SpotifyIcon, MailIcon, EthereumIcon } from "../Icons/Icons";
 
-// <FooterLink href="https://svmplr.com">← Back to SVMPLR.com</FooterLink>
-/*
 const Footer = () => {
-
   return (
     <div className="py-4 px-8 w-full flex flex-col md:flex-row items-center justify-between space-y-1 md:space-y-0 mt-auto text-center">
-      <div className="space-x-4">
-        <FooterLink href={"https://nft.gamestop.com/user/SVMPLR?tab=created"}>
-          <p className="hidden md:inline-block"></p>
-          <EthereumIcon className="w-5 h-5" />
-        </FooterLink>
-        <FooterLink href={"https://twitter.com/svmplr_wav"}>
-          <p className="hidden md:inline-block"></p>
-          <TwitterIcon className="w-5 h-5" />
-        </FooterLink>
-        <FooterLink href={"https://open.spotify.com/artist/0OhUn4CLob6B37uNElJ1XB?si=vGkcSeoYRpWolcGMBd6Iug&nd=1"}>
-          <p className="hidden md:inline-block"></p>
-          <SpotifyIcon className="w-5 h-5" />
-        </FooterLink>
-        <FooterLink href={"mailto:svmplrofficial@gmail.com"}>
-          <p className="hidden md:inline-block"></p>
-          <MailIcon className="w-5 h-5" />
-        </FooterLink>
-      </div>
+      <p className="text-white/40">
+        <FooterLink href="https://svmplr.com">← Back to SVMPLR.com</FooterLink>
+      </p>
     </div>
   );
 };
-
-*/
 
 export default Footer;
